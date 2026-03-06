9a32f2292f374a79bdf9a324e493dc620251fb73
rakeshyadhav21 <rakeshyadhav329@gmail.com>
2026-03-06 23:03:00 +0530

    remove env file and add gitignore

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
     try{
         await mongoose.connect(process.env.MONGO_URL);
-        console.log("MongoDB connected successfully.");
+        console.log(`MongoDB connected successfully: ${conn.connection.host}`);
     }
     catch(err){
         console.error("MongoDB connection failed.",err)
